12d14ea60b95c0561d02cdd7e8d53d147da7c33a
yaolx <64489456@qq.com>
2022-12-03 21:47:44 +0800

    feat： header组件

--- src/renderer/src/components/layout/header.tsx
+++ src/renderer/src/components/layout/header.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
 
-import { SmileTwoTone } from '@ant-design/icons'
+import { SmileTwoTone, createFromIconfontCN } from '@ant-design/icons'
 import { Layout } from 'antd'
 
 import styles from './styles/index.module.less'
 
 const { Header } = Layout
+const IconFont = createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/c/font_3807038_qj3err6v8j.js'
+})
 function HeaderLayout() {
   return (
     <Header className={styles.header}>
-      <SmileTwoTone style={{ fontSize: 20 }} />
+      <div className={styles.logo}>
+        <SmileTwoTone style={{ fontSize: 20 }} />
+      </div>
+      <div className={styles.drag_area}></div>
+      <div className={styles.oper_btns}>
+        <IconFont type="icon-suoxiao" className={styles.btn} />
+        <IconFont type="icon-fangda" className={styles.btn} />
+        <IconFont type="icon-guanbi" className={styles.btn} />
+      </div>
     </Header>
   )
 }
